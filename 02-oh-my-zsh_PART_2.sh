0a0964dd38398aa3aff60298685d7925a91ec0a9
Josh Walters <josh.walters@outlook.com>
2024-05-16 10:23:45 -0500

    fixed path update script

--- 02-oh-my-zsh_PART_2.sh
+++ 02-oh-my-zsh_PART_2.sh
@@ -5,7 +5,7 @@ cp .zsh_aliases ~/
 mkdir -p ~/.local/share/bin
 
 sed -i 's/# export PATH=/export PATH=/' ~/.zshrc
-set -i -E 's/export PATH=(.*)\:\$PATH/export PATH=\1\:\$HOME\/.local/share/bin\:\$PATH'
+sed -i -E 's/export PATH=(.*)\:\$PATH/export PATH=\1\:\$HOME\/.local\/share\/bin\:\$PATH/' ~/.zshrc
 
 echo "" >>~/.zshrc
 echo "source \$HOME/.zsh_aliases" >>~/.zshrc
